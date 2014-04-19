592a5163003b29f9d5095e9660fc3b18c04214e3
Fredrik Kock <freko247@gmail.com>
2014-04-19 14:59:14 +0200

    Added stubs for creating a huddle, not actually storing any data yet.

--- www/js/functions.js
+++ www/js/functions.js
@@ -48,3 +48,38 @@ function removeSkill() {
     $(this).closest('li').remove();
     $('ul').listview('refresh');
 }
+
+function addHuddleTag() {
+    $('#huddle-tag-list').append($('<li/>', { //here appending `<li>`
+        'data-icon': 'delete',
+        'class': 'huddle-tag-item',
+    }).append($('<a/>', { //here appending `<a>` into `<li>`
+        'href': '#',
+        'data-transition': 'slide',
+        'text': jQuery("#huddle-tag-control-group").val()
+    })));
+
+    $('ul').listview('refresh');
+}
+
+function createHuddle() {
+    var onSuccess = function(position) {
+        alert('Latitude: ' + position.coords.latitude + '\n' +
+            'Longitude: ' + position.coords.longitude + '\n' +
+            'Altitude: ' + position.coords.altitude + '\n' +
+            'Accuracy: ' + position.coords.accuracy + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+            'Heading: ' + position.coords.heading + '\n' +
+            'Speed: ' + position.coords.speed + '\n' +
+            'Timestamp: ' + new Date(position.timestamp) + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
